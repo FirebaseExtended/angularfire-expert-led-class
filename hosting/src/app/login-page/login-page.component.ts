cfb13264fff29fc5a41ff1371e32db1f3864ff0a
David East <deast@google.com>
2022-10-16 13:59:36 -0400

    login

--- hosting/src/app/login-page/login-page.component.ts
+++ hosting/src/app/login-page/login-page.component.ts
@@ -14,47 +14,59 @@
  limitations under the License.
  */
 
-import { Component, inject, OnInit } from '@angular/core';
-import {
-  Auth,
-  authState,
-  GoogleAuthProvider,
-  signInAnonymously,
-  signInWithRedirect,
-  signOut,
-  User,
-  getRedirectResult,
-  getAdditionalUserInfo,
-  UserCredential,
-} from '@angular/fire/auth';
-import { doc, Firestore, setDoc } from '@angular/fire/firestore';
-import { Router } from '@angular/router';
-import { Observable, of } from 'rxjs';
-import { ResumeUser } from '../models/resume.model';
-import { ResumeService } from '../services/resume.service';
-
-@Component({
-  selector: 'app-login-page',
-  templateUrl: './login-page.component.html',
-  styleUrls: ['./login-page.component.css'],
-})
-export class LoginPageComponent implements OnInit {
-  private router = inject(Router);
-  user$: Observable<Partial<User> | null> = of(null)
-
-  ngOnInit() {
-    // Detect redirect state
-  }
-
-  loginGuest() {
-    // Login anonymously
-  }
-
-  login() {
-    // Login with Google
-  }
-
-  private redirectToEditPage(uid: string) {
-    this.router.navigate([`edit/${uid}`]);
-  }
-}
+ import { Component, inject, OnInit } from '@angular/core';
+ import {
+   Auth,
+   authState,
+   GoogleAuthProvider,
+   signInAnonymously,
+   signInWithRedirect,
+   signOut,
+   User,
+   getRedirectResult,
+   getAdditionalUserInfo,
+   UserCredential,
+ } from '@angular/fire/auth';
+ import { doc, Firestore, setDoc } from '@angular/fire/firestore';
+ import { Router } from '@angular/router';
+ import { Observable, of } from 'rxjs';
+ import { ResumeUser } from '../models/resume.model';
+ import { ResumeService } from '../services/resume.service';
+ 
+ @Component({
+   selector: 'app-login-page',
+   templateUrl: './login-page.component.html',
+   styleUrls: ['./login-page.component.css'],
+ })
+ export class LoginPageComponent implements OnInit {
+   private router = inject(Router);
+   user$: Observable<Partial<User> | null> = of(null)
+   auth = inject(Auth);
+   private googleAuthProvider = new GoogleAuthProvider();
+   resumeService = inject(ResumeService);
+ 
+   ngOnInit() {
+     // Detect redirect state
+     getRedirectResult(this.auth).then(result => {
+       if (!result) return;
+       this.resumeService.createEmptyResume(result.user).then(() => {
+         this.redirectToEditPage(result.user.uid);
+       })
+     });
+   }
+ 
+   loginGuest() {
+     // Login anonymously
+     signInAnonymously(this.auth);
+   }
+ 
+   login() {
+     // Login with Google
+     signInWithRedirect(this.auth, this.googleAuthProvider);
+   }
+ 
+   private redirectToEditPage(uid: string) {
+     this.router.navigate([`edit/${uid}`]);
+   }
+ }
+ 
